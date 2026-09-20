5d2f7b3d573505f05919222fff9d7ca50fa6098f
ezygotravel <travelsezygo@gmail.com>
2026-09-20 21:47:10 +0530

    Delete supabase directory

--- supabase/image-storage-fix.sql
+++
@@ -1,6 +0,0 @@
--- EzyGo image serving repair
--- Safe to run once in Supabase SQL Editor.
-
-insert into storage.buckets (id, name, public)
-values ('site-media', 'site-media', true)
-on conflict (id) do update set public = true;
