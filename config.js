be44be07ea76d9943f74849c29caf1efaf12e6ae
ezygotravel <travelsezygo@gmail.com>
2026-09-20 21:46:43 +0530

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.EZYGO_CONFIG = Object.freeze({
-  SUPABASE_URL: 'https://eqtitceuapjuwockosnm.supabase.co',
-  SUPABASE_PUBLISHABLE_KEY: 'sb_publishable_JdC2juUOVDRVe0Rt-OC7AA_IpbzZU8Z',
-  STORAGE_BUCKET: 'site-media'
-});
